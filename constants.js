6901082be6f275602aea7b76aa2b73fdb6a15ec3
Joao Alexandre dos Santos Lopes <joao.lopes@bndes.gov.br>
2025-04-17 19:47:08 -0300

    Implementação inicial de script para governança

--- constants.js
+++ constants.js
@@ -40,7 +40,16 @@ const NODE_RULES_V2_ABI = [
     'function getNumberOfNodesByOrg(uint orgId) external view returns (uint)',
     'function getNodes(uint pageNumber, uint pageSize) external view returns (tuple(bytes32 enodeHigh, bytes32 enodeLow, uint8 nodeType, string name, uint orgId, bool active)[] memory)',
     'function getNodesByOrg(uint orgId, uint pageNumber, uint pageSize) external view returns (tuple(bytes32 enodeHigh, bytes32 enodeLow, uint8 nodeType, string name, uint orgId, bool active)[] memory)'
-]
+];
+const GOVERNANCE_ABI = [
+    'function createProposal(address[] calldata targets, bytes[] memory calldatas, uint blocksDuration, string calldata description) public',
+    'function cancelProposal(uint proposalId, string calldata reason) public',
+    'function castVote(uint proposalId, bool approve) public',
+    'function executeProposal(uint proposalId) public',
+    'function getProposal(uint proposalId) public view returns (tuple(uint id, uint proponentOrgId, address[] targets, bytes[] calldatas, uint blocksDuration, string description, uint creationBlock, uint8 status, uint8 result, uint[] organizations, uint8[] votes, string cancelationReason) memory)',
+    'function getNumberOfProposals() public view returns (uint)',
+    'function getProposals(uint pageNumber, uint pageSize) public view returns (tuple(uint id, uint proponentOrgId, address[] targets, bytes[] calldatas, uint blocksDuration, string description, uint creationBlock, uint8 status, uint8 result, uint[] organizations, uint8[] votes, string cancelationReason)[] memory)'
+];
 const RULES_CONTRACT = '0x72756c6573000000000000000000000000000000000000000000000000000000';
 const NODE_INGRESS_ADDRESS = '0x0000000000000000000000000000000000009999';
 const ACCOUNT_INGRESS_ADDRESS = '0x0000000000000000000000000000000000008888';
@@ -51,6 +60,7 @@ module.exports = {
     ORGANIZATION_ABI: ORGANIZATION_ABI,
     ACCOUNT_RULES_V2_ABI: ACCOUNT_RULES_V2_ABI,
     NODE_RULES_V2_ABI: NODE_RULES_V2_ABI,
+    GOVERNANCE_ABI: GOVERNANCE_ABI,
     RULES_CONTRACT: RULES_CONTRACT,
     NODE_INGRESS_ADDRESS: NODE_INGRESS_ADDRESS,
     ACCOUNT_INGRESS_ADDRESS: ACCOUNT_INGRESS_ADDRESS
---
+++ governance.js
@@ -0,0 +1,84 @@
+const ethers = require('ethers');
+const { setup, getParameter, diagnostics, getSigner, getFunctionArgs, verifyArgsLength, help, getProposalStatus, getProposalResult, getVote } = require('./util.js');
+const { GOVERNANCE_ABI } = require('./constants.js');
+
+const syntax = {
+    'createProposal': 'createProposal ???targets??? ???calldatas???, <blocksDuration>, <description>',
+    'cancelProposal': 'cancelProposal <proposalId> <reason>',
+    'castVote': 'castVote <proposalId> <approve>',
+    'executeProposal': 'executeProposal <proposalId>',
+    'getProposal': 'getProposal <proposalId>',
+    'getNumberOfProposals': 'getNumberOfProposals',
+    'getProposals': 'getProposals <pageNumber> <pageSize>'
+};
+
+function formatTargets(targets) {
+    let formated = '';
+    for(t of targets) {
+        formated = formated + '\n  ' + t;
+    }
+    return formated;
+}
+
+function formatCalldatas(calldatas) {
+    let formated = '';
+    for(c of calldatas) {
+        formated = formated + '\n  ' + c;
+    }
+    return formated;
+}
+
+function formatVotes(votes) {
+    return votes.map(v => getVote(v));
+}
+
+const governance = {
+    'getProposal': async function (contract, func, args) {
+        verifyArgsLength(1, func, args, syntax[func]);
+        const propId = args[0];
+        const prop = await contract.getProposal(propId);
+        console.log(`\nProposta ${prop[0]}:\n Proponent: ${prop[1]}\n Targets: ${formatTargets(prop[2])}\n Calldatas: ${formatCalldatas(prop[3])}\n Blocks duration: ${prop[4]}\n Description: ${prop[5]}\n Creation Block: ${prop[6]}\n Status: ${getProposalStatus(prop[7])}\n Result: ${getProposalResult(prop[8])}\n Organizations: ${prop[9]}\n Votes: ${formatVotes(prop[10])}\n Cancelation reason: ${prop[11]}`);
+    },
+    'getNumberOfProposals': async function (contract, func, args) {
+        verifyArgsLength(0, func, args, syntax[func]);
+        const numProposals = await contract.getNumberOfProposals();
+        console.log(`\nQuantidade de propostas: ${numProposals}`);
+    },
+    'getProposals': async function (contract, func, args) {
+        verifyArgsLength(2, func, args, syntax[func]);
+        const pageNumber = args[0];
+        const pageSize = args[1];
+        const props = await contract.getProposals(pageNumber, pageSize);
+        console.log(`\nId;Proponent;Targets;Calldatas;Blocks duration;Description;Creation block;Status;Result;Organizations;Votes;Cancelation reason]:`);
+        for(prop of props) {
+            console.log(`${prop[0]};${prop[1]};${prop[2]};${prop[3]};${prop[4]};${prop[5]};${prop[6]};${getProposalStatus(prop[7])};${getProposalResult(prop[8])};${prop[9]};${formatVotes(prop[10])};${prop[11]}`);
+        }
+    },
+};
+
+async function run() {
+    await diagnostics();
+    const governanceAddress = getParameter('governanceAddress');
+    console.log(`Governance: ${governanceAddress}\n`);
+    
+    const governanceContract = new ethers.Contract(governanceAddress, GOVERNANCE_ABI, getSigner());
+    const [func, args] = getFunctionArgs(__filename);
+
+    if(func.toLowerCase() === 'help') {
+        help(syntax);
+        return;
+    }
+
+    console.log('==================================================');
+    console.log(`Governance.${func}(${args})`);
+    console.log('==================================================');
+    
+    if(governance[func] == undefined) {
+        console.log(`Função ${func} não encontrada`);
+        process.exit(2);
+    }
+    await governance[func](governanceContract, func, args);
+}
+
+setup();
+run();
