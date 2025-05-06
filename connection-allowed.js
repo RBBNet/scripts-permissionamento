313719bf90864308dcd5971e9a82fbedc76995e3
Joao Alexandre dos Santos Lopes <joao.lopes@bndes.gov.br>
2025-05-06 19:28:44 -0300

    Melhorias no script para teste de conectividade entre nós

--- connection-allowed.js
+++ connection-allowed.js
@@ -13,21 +13,24 @@ async function run() {
     const nodesContract = new ethers.Contract(nodeRulesV2Address, NODE_RULES_V2_ABI, getSigner());
 
     const numArgs = process.argv.length;
-    if(numArgs != 4) {
-        console.log(`Utilizar sintaxe: node ${path.basename(__filename)} <enodeHigh> <enodeLow>`);
+    if(numArgs != 4 && numArgs != 6) {
+        console.log(`Utilizar sintaxe: node ${path.basename(__filename)} <sourceEnodeHigh> <sourceEnodeLow> {destEnodeHigh} {destEnodeLow}`);
+        console.log(`Caso os parâmtros {destEnodeHigh} {destEnodeLow} não sejam informados, eles serão preenchidos com os mesmos valores dos parâmetros <sourceEnodeHigh> <sourceEnodeLow>`);
         process.exit(1);
     }
-    const enodeHigh = process.argv[2];
-    const enodeLow = process.argv[3];
+    const sourceEnodeHigh = process.argv[2];
+    const sourceEnodeLow = process.argv[3];
+    const destEnodeHigh = numArgs == 6 ? process.argv[4] : sourceEnodeHigh;
+    const destEnodeLow = numArgs == 6 ? process.argv[5] : sourceEnodeLow;
 
     console.log('==================================================');
     try {
-        const result = await nodesContract.connectionAllowed(enodeHigh, enodeLow, ZEROED_BYTES_16, 0, enodeHigh, enodeLow, ZEROED_BYTES_16, 0);
+        const result = await nodesContract.connectionAllowed(sourceEnodeHigh, sourceEnodeLow, ZEROED_BYTES_16, 0, destEnodeHigh, destEnodeLow, ZEROED_BYTES_16, 0);
         const allowed = (result == CONNECTION_ALLOWED);
-        console.log(`connectionAllowed(${enodeHigh}, ${enodeLow}, ${ZEROED_BYTES_16}, 0, ${enodeHigh}, ${enodeLow}, ${ZEROED_BYTES_16}, 0) - ${allowed}`);
+        console.log(`connectionAllowed(${sourceEnodeHigh}, ${sourceEnodeLow}, ${ZEROED_BYTES_16}, 0, ${destEnodeHigh}, ${destEnodeLow}, ${ZEROED_BYTES_16}, 0) - ${allowed}`);
     }
     catch(error) {
-        console.log(`Erro na verificação de conectividade do nó ${enodeHigh} ${enodeLow}: ${error.message}`);
+        console.log(`Erro na verificação de conectividade: ${error.message}`);
     }
 }
 
