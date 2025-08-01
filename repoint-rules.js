3268b18febf1385b52902ea2b5b962ee390cabf9
Joao Alexandre dos Santos Lopes <joao.lopes@bndes.gov.br>
2025-08-01 15:30:46 -0300

    Refatoração para adaptar o script ao contexto do repositório

--- repoint-rules.js
+++ repoint-rules.js
@@ -1,39 +1,36 @@
 const { ethers } = require("ethers");
-require('dotenv').config();
-
-const ingressAbi = [
-    "function setContractAddress(bytes32 name, address addr)"
-];
+const { setup, diagnostics, getParameter, getSigner, handleTx } = require('./util.js');
+const { INGRESS_ABI, RULES_CONTRACT } = require('./constants.js');
 
 async function main(){
+    await diagnostics();
     console.log("Iniciando reponteiramento de regras");
-    const provider = new ethers.JsonRpcProvider(process.env.JSON_RPC_URL);
-    const signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
-    console.log(`Usando o endereço ${signer.address} para enviar as transações`);
-    const accountIngressAddr = process.env.ACCOUNT_INGRESS_ADDRESS;
-    const nodeIngressAddr = process.env.NODE_INGRESS_ADDRESS;
-    const newAccountRulesAddr = process.env.ACCOUNT_RULES_V2_ADDRESS;
-    const newNodeRulesAddr = process.env.NODE_RULES_V2_ADDRESS;
-    const rulesContractName = "0x72756c6573000000000000000000000000000000000000000000000000000000";
-    if(!newAccountRulesAddr || !newNodeRulesAddr){
-        throw new Error("Endereços de contrato da Gen02 não encontrados no .env");
-    }
-    const accountIngress = new ethers.Contract(accountIngressAddr, ingressAbi, signer);
-    const nodeIngress = new ethers.Contract(nodeIngressAddr, ingressAbi, signer);
-    console.log(`\n Reponteirando AccountIngress (${accountIngressAddr} para ${newAccountRulesAddr})`);
-    const tx1 = await accountIngress.setContractAddress(rulesContractName, newAccountRulesAddr);
-    console.log(`Transação enviada: ${tx1.hash}`);
-    await tx1.wait();
-    console.log("Reponteiramento do AccountIngress concluido.");
 
-    console.log(`\n Reponteirando NodeIngress (${nodeIngressAddr} para ${newNodeRulesAddr})`);
-    const tx2 = await nodeIngress.setContractAddress(rulesContractName, newNodeRulesAddr);
-    console.log(`Transação enviada: ${tx2.hash}`);
-    await tx2.wait();
-    console.log("Reponteiramento do NodeIngress concluido.");
+    const accountIngressAddr = getParameter('ACCOUNT_INGRESS_ADDRESS');
+    const nodeIngressAddr = getParameter('NODE_INGRESS_ADDRESS');
+    const newAccountRulesAddr = getParameter('ACCOUNT_RULES_V2_ADDRESS');
+    const newNodeRulesAddr = getParameter('NODE_RULES_V2_ADDRESS');
+
+    if(!newAccountRulesAddr){
+        throw new Error("Variável ACCOUNT_RULES_V2_ADDRESS não configurada");
+    }
+    if(!newNodeRulesAddr){
+        throw new Error("Variável NODE_RULES_V2_ADDRESS não configurada");
+    }
+
+    const accountIngress = new ethers.Contract(accountIngressAddr, INGRESS_ABI, getSigner());
+    const nodeIngress = new ethers.Contract(nodeIngressAddr, INGRESS_ABI, getSigner());
+
+    console.log(`\nReponteirando AccountIngress (de ${accountIngressAddr} para ${newAccountRulesAddr})`);
+    const tx1 = await accountIngress.setContractAddress(RULES_CONTRACT, newAccountRulesAddr);
+    handleTx(tx1);
+    console.log("\nReponteiramento do AccountIngress concluido.");
+
+    console.log(`\nReponteirando NodeIngress (de ${nodeIngressAddr} para ${newNodeRulesAddr})`);
+    const tx2 = await nodeIngress.setContractAddress(RULES_CONTRACT, newNodeRulesAddr);
+    handleTx(tx2);
+    console.log("\nReponteiramento do NodeIngress concluido.");
 }
 
-main().catch((error) => {
-    console.error(error);
-    process.exitCode = 1;
-});
+setup();
+main();
